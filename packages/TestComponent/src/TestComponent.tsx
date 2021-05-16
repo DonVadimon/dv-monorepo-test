bb037cd0055982289ca2637110aaaed9b80bba42
Vadim Khizhnyakov <provcydia@gmail.com>
2021-05-16 14:25:27 +0300

    feat(testcomponent): inner text of Test component changed to "Im working"

--- packages/TestComponent/src/TestComponent.tsx
+++ packages/TestComponent/src/TestComponent.tsx
@@ -7,7 +7,7 @@ import './TestComponent.css';
 const TestComponent: React.FC<ITestComponentProps> = ({ theme = 'light' }) => (
     <div className={['test-component', `test-component-${theme}`].join(' ')} data-testid="test-component">
         <h1>Test component</h1>
-        <h2>Work pls</h2>
+        <h2>Im working</h2>
     </div>
 );
 
