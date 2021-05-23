04095d912c46ba9ca878dfda33d01100ac62b586
Vadim Khizhnyakov <provcydia@gmail.com>
2021-05-23 13:07:02 +0300

    feat: change two

--- packages/TestComponent/src/TestComponent.tsx
+++ packages/TestComponent/src/TestComponent.tsx
@@ -8,7 +8,7 @@ const TestComponent: React.FC<ITestComponentProps> = ({ theme = 'light' }) => (
     <div className={['test-component', `test-component-${theme}`].join(' ')} data-testid="test-component">
         <h1>Test component</h1>
         <h2>Im working</h2>
-        <h3>Changes for changes for squash</h3>
+        <h3>Changes for changes for squashhhhhhh</h3>
     </div>
 );
 
